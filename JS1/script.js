8da4cdf66ab703e39d17c46418c98ca0fd671a2b
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2020-12-16 19:16:36 +0300

    first and second js tasks

---
+++ JS1/script.js
@@ -0,0 +1,22 @@
+while (true) {
+    let first = prompt('Введите первое число');
+    let second = prompt('Введите второе число');
+
+    if (first != first * 1) {
+        alert('Первый ввод - не число');
+        break;
+    }
+
+    if (second != second * 1) {
+        alert('Второй ввод - не число');
+        break;
+    }
+
+    if (first === second) {
+        alert('Числа равны');
+    } else if (first > second) {
+        alert('Первое число больше');
+    } else {
+        alert('Второе число больше');
+    }
+}
---
+++ JS2/script.js
@@ -0,0 +1,21 @@
+let numOfStoreys = prompt('Введите количество этажей');
+let numOfEntrances = prompt('Введите количество подъездов');
+let numOfApartments = prompt('Введите количество квартир на этаже');
+let apartment = prompt('Введите номер квартиры');
+
+if (numOfStoreys != numOfStoreys * 1 || numOfEntrances != numOfEntrances * 1 ||
+    numOfApartments != numOfApartments * 1 || apartment != apartment * 1) {
+    alert('Что-то пошло не так, введите данные повторно');
+
+} else {
+    let entrance = Math.floor((Math.floor(apartment / numOfApartments) + 1) / numOfStoreys) + 1;
+
+    if (entrance > numOfEntrances) {
+        alert(`Введено неверное количество подъездов. Данная квартира должна находиться в подъезде ${entrance}`);
+    } else {
+        alert(`Нужна квартира находится в подъезде ${entrance}`);
+    }
+}
+
+
+
