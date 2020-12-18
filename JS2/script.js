65db403ca9a121450f2c63ccf36f652bf3733fbe
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2020-12-18 12:03:43 +0300

    added conditions for checking integers and 0, fixed answers for last appartments

--- JS2/script.js
+++ JS2/script.js
@@ -7,6 +7,14 @@ if (numOfStoreys != numOfStoreys * 1 || numOfEntrances != numOfEntrances * 1 ||
     numOfApartments != numOfApartments * 1 || apartment != apartment * 1) {
     alert('Что-то пошло не так, введите данные повторно');
 
+} else if (!Number.isInteger(+numOfStoreys) ||!Number.isInteger(+numOfEntrances) ||
+    !Number.isInteger(+numOfApartments) || !Number.isInteger(+apartment)) {
+    alert('Числа должны быть целыми');
+} else if (+numOfStoreys === 0 || +numOfEntrances === 0 || +numOfApartments === 0 ||
+    +apartment === 0) {
+    alert('Номера не должны равняться 0');
+} else if (+apartment === numOfStoreys * numOfApartments * numOfEntrances) {
+    alert(`Нужна квартира находится в подъезде ${numOfEntrances}`);
 } else {
     let entrance = Math.floor((Math.floor(apartment / numOfApartments) + 1) / numOfStoreys) + 1;
 
