8746ded7df980bcf26d0abe88f8450e871ee7b09
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2020-12-21 12:16:18 +0300

    fixed wrong answers

--- JS2/script.js
+++ JS2/script.js
@@ -13,8 +13,8 @@ if (numOfStoreys != numOfStoreys * 1 || numOfEntrances != numOfEntrances * 1 ||
 } else if (+numOfStoreys === 0 || +numOfEntrances === 0 || +numOfApartments === 0 ||
     +apartment === 0) {
     alert('Номера не должны равняться 0');
-} else if (+apartment === numOfStoreys * numOfApartments * numOfEntrances) {
-    alert(`Нужна квартира находится в подъезде ${numOfEntrances}`);
+} else if (+apartment % +numOfStoreys === 0) {
+    alert(`Нужна квартира находится в подъезде ${+apartment / +numOfStoreys}`)
 } else {
     let entrance = Math.floor((Math.floor(apartment / numOfApartments) + 1) / numOfStoreys) + 1;
 
