434c2b9800356558f883dce2c6ba9625c7bcb126
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2020-12-21 13:31:00 +0300

    done task 7(2.3)

---
+++ JS7/script.js
@@ -0,0 +1,11 @@
+function loop () {
+    for (var i = 0; i < 10; i++) {
+        var x = i;
+    }
+
+    alert(`X, находящийся в цикле: ${x}`);
+}
+
+loop();
+var x = 777;
+alert(`X, находящийся вне цикла: ${x}`);
