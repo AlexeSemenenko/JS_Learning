08636240e5340e267bd7fe486a50bcb77d414c14
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2020-12-23 16:31:45 +0300

    fixed adding the last element to the array

--- JS8/script.js
+++ JS8/script.js
@@ -1,23 +1,30 @@
 const range = (start, finish, step = 1) => {
-    let array = [];
+    let array = []
+
     if (isNaN(start) || isNaN(finish) || isNaN(step)) {
-        return 'Проверьте тип введенных данных';
+        return 'Проверьте тип введенных данных'
     } else if (step === 0){
-        return 'Шаг не может равнять нулю';
-    } else if (step >= 1 && start >= finish) {
-        return 'Старт не может быть больше финиша при положительном шаге';
-    } else if (step <= -1 && start <= finish) {
-        return 'Старт не может быть меньше финиша при отрицательном шаге';
-    } else {
-        for (let i = 0; i <= Math.floor(Math.abs((start - finish) / step)); i++) {
-            array[i] = start + step * i;
-        }
-        return array;
+        return 'Шаг не может равнять нулю'
+    } else if (step > 0 && start >= finish) {
+        return 'Старт не может быть больше финиша при положительном шаге'
+    } else if (step < 0 && start <= finish) {
+        return 'Старт не может быть меньше финиша при отрицательном шаге'
     }
+
+    for (let i = 0; i <= Math.floor(Math.abs((start - finish) / step)); i++) {
+        array[i] = start + step * i
+    }
+
+    if (!array.includes(finish)) {
+        array.push(finish)
+    }
+
+    return array
 }
 
-alert(range("avd", 5));
-alert(range(1, 5, -1));
-alert(range(5, 1));
-alert(range(1, 5));
-alert(range(5, 1, -1));
+alert(range("avd", 5))
+alert(range(1, 5, -1))
+alert(range(5, 1))
+alert(range(1, 5))
+alert(range(5, 1, -1))
+alert(range(1, 6, 2))
